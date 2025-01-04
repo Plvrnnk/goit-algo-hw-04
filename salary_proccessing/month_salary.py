2ef25b15d875be6c91f783885d2268b41e382873
Polina Lavrynenko <125290836+Plvrnnk@users.noreply.github.com>
2025-01-04 15:48:04 +0200

    1 task

---
+++ salary_proccessing/month_salary.py
@@ -0,0 +1,19 @@
+def total_salary(path):
+    salary_list = []
+    total = 0
+    try:
+        with open(path, 'r') as fl:
+            lines = fl.readlines()
+            for line in lines:
+                num = line.strip().split(',')
+                salary_list.append(int(num[1]))
+    except FileNotFoundError:
+        print('File is not found')
+    
+    for salary in salary_list:
+        total += salary
+        average = int(total / len(salary_list))
+    return total, average
+
+total, average = total_salary('./salary.txt')
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
